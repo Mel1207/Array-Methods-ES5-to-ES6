3740b6edf457a75836b0688420703b19dbff0148
Mel <meljohnpualon@gmail.com>
2022-03-11 04:18:29 +0800

    added another 4 array methods

--- app.js
+++ app.js
@@ -13,23 +13,44 @@ const items = [
 const filteredItems = items.filter(item => {
     return item.price <= 100
 }) 
-console.log(filteredItems)
+// console.log(filteredItems)
 
 // MAP ARRAY
 const itemNames = items.map(item => {
     return item.name
 })
-
-console.log(itemNames)
+// console.log(itemNames)
 
 // FIND ARRAY
 const foundItem = items.find(item => {
     return item.name === 'Album'
 })
-
-console.log(foundItem)
+// console.log(foundItem)
 
 // FOR EACH
 items.forEach(item => {
-    console.log(item.name)
+    // console.log(item.name)
 })
+
+// SOME , returns a boolean value upon looking for items in array
+const hasExpensiveItems = items.some(item => {
+    return item.price > 1000
+})
+// console.log(hasExpensiveItems)
+
+// EVERY, returns a boolean value uppon checking an array if conditions meet
+const hasFreeItems = items.every(item => {
+    return item < 0
+})
+// console.log(hasFreeItems)
+
+// REDUCE
+const total = items.reduce((currentTotal, item) => {
+    return item.price + currentTotal
+}, 0)
+console.log(total)
+
+// INCLUDES
+const numbers = [1, 2, 3, 4, 5]
+const numberIncludes = numbers.includes(2)
+console.log(numberIncludes)
